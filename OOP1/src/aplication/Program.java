cc887aac3b183db7bbf4e757814e9f45aa4b0a65
kilderilo <kilderilopes@gmail.com>
2022-04-28 11:04:08 -0300

    Commit 28/04/22

--- OOP1/src/aplication/Program.java
+++ OOP1/src/aplication/Program.java
@@ -1,2 +1,41 @@
-package aplication;public class Program {
+package aplication;
+
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class Program {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Digite os lados do triangulo X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Digite os lados do triangulo Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Area do triangulo X: %.2f%n", areaX);
+        System.out.printf("Area do triangulo Y: %.2f%n", areaY);
+
+        if(areaX > areaY){
+            System.out.printf("Triangulo de maior area: X, %.2f", areaX);
+        }
+        else{
+            System.out.printf("Triangulo de maior area: Y, %.2f", areaY);
+        }
+
+        sc.close();
+    }
+
 }

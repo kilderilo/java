0c3cca917ef92c5fa2d37d4dce19d262745a2351
kilderilo <kilderilopes@gmail.com>
2022-05-10 17:30:21 -0300

    Commit 10/05

---
+++ dio/bankline-api/src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -0,0 +1,51 @@
+package com.dio.santander.banklineapi.model;
+
+import java.time.LocalDateTime;
+
+public class Movimentacao {
+    private int id;
+    private LocalDateTime dataHora;
+    private String descricao;
+    private double valor;
+    private MovimentacaoTipo tipo;
+
+    public MovimentacaoTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(MovimentacaoTipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}

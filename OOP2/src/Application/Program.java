f9490dcc9365eddae6ddc9f37567f8260abbbe3a
kilderilo <kilderilopes@gmail.com>
2022-05-18 12:35:32 -0300

    Inserção de comentários.

--- OOP2/src/Application/Program.java
+++ OOP2/src/Application/Program.java
@@ -2,15 +2,16 @@ package Application;
 
 import java.util.Scanner;
 
-import entities.Product;
+import entities.Product; //importação da classe Product
 
 public class Program{
 
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-            
+            //instanciação da classe Product, criando a variável "product"
             Product product = new Product();
-            
+
+            //Entrada de dados
             System.out.println("Enter product data: ");
             System.out.print("Name: ");
             product.name = sc.nextLine();
@@ -19,23 +20,27 @@ public class Program{
             System.out.print("Quantity: ");
             product.quantity = sc.nextInt();
 
+            //primeira saída com o resultado da entrada de dados
             System.out.println();
             System.out.println("Product data: " + product);
 
+            //Bloco que adiciona quantidades ao estoque
             System.out.println();
             System.out.println("Enter the product quantity to be added in stock: ");
             int quantity = sc.nextInt();
             product.addProducts(quantity);
 
+            //Saída com o valor atualizado de quantidade e valor total do estoque
             System.out.println();
             System.out.println("Updated data: " + product);
 
-
+            //Bloco que remove quantidades do estoque
             System.out.println();
             System.out.println("Enter the product quantity to be removed from stock: ");
             quantity = sc.nextInt();
             product.removeProducts(quantity);
 
+            //Saída atualizada da situação do estoque
             System.out.println();
             System.out.println("Updated data: " + product);
 
--- OOP2/src/entities/Product.java
+++ OOP2/src/entities/Product.java
@@ -1,22 +1,27 @@
 package entities;
 
+//Criação da classe Product
 public class Product {
     public String name;
     public double price;
     public int quantity;
 
+    //Construção do método que calcula o total no estoque
     public double totalValueInstock(){
+
         return price * quantity;
     }
-
+    //Método que adiciona produtos no estoque
     public void addProducts(int quantity){
+
         this.quantity += quantity;
     }
-
+    //Método que remove produtos do estoque
     public void removeProducts(int quantity){
+
         this.quantity -= quantity;
     }
-
+    //Saída de tela que mostra as informações da variável "name" formatadas com o método "toString"
     public String toString(){
         return name
             + ", $ "
